7118f52c4c501bb130a0cf4d838f30a419132f78
他是个疯子 <tashigefengzi@tashigefengzis-iMac.local>
2019-04-22 21:42:02 +0800

    他是个疯子

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/插件名称/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852359/快捷表情/g"  cydia副本 sileo副本.json
 #描述
-perl -p -i -e "s/852360/插件描述！/g"  cydia副本 sileo副本.json
+perl -p -i -e "s/852360/为iphoneX键盘下方添加快捷表情/g"  cydia副本 sileo副本.json
 #支持系统
 perl -p -i -e "s/852362/ios:11-12/g"  cydia副本 sileo副本.json
 
 #插件标识符
-mv cydia副本 beta
-mv sileo副本.json beta.json
+mv cydia副本 io.fengzibarmoji
+mv sileo副本.json io.fengzibarmoji.json
 
 
 cp cydia样本 cydia副本
